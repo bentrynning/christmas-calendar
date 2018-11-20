96732d355d74721ec4c4828363996190a88b862c
Bent <bent.rynning@gmail.com>
2018-11-20 22:58:11 +0100

    Added firestore

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import Calendar from './components/Calendar';
 import Canvas from './components/Canvas';
 import './App.css';
+import config from './config.json';
+
+firebase.initializeApp(config);
+
+const firestore = firebase.firestore();
+const datesDocRef = firestore.doc('calendar/dates');
+const usersDocRef = firestore.doc('calendar/users');
 
 class App extends Component {
   state = {
-    names: ['Bent', 'Håkon', 'Jon', 'Mira', 'Martin', 'Lorita', 'Thomas', 'Julij', 'Anders', 'Evind', 'Emil', 'Eli', 'Kristin', 'Marcus', 'Thin', 'Sigurd', 'Fredrik']
+    names: ['bent', 'Håkon', 'Jon', 'Mira', 'Martin', 'Lorita', 'Thomas', 'Julij', 'Anders', 'Evind', 'Emil', 'Eli', 'Kristin', 'Marcus', 'Thin', 'Sigurd', 'Fredrik'],
+    openDates: {}
   }
+
+  async componentWillMount() {
+    datesDocRef.onSnapshot((datesDoc) => {
+      if(datesDoc && datesDoc.exists) {
+        this.setState({
+          openDates: datesDoc.data()
+        })
+       }
+    });
+  }
+
   openCurrentDate = () => {
-    
+    const doDaysDate = new Date().getDate();
+    const selectedNumber = Math.floor(Math.random() * (this.state.names.length + 1)) ;
+    const doDaysName = this.state.names[selectedNumber];
+    datesDocRef.set({
+      ...this.state.openDates,
+      [doDaysDate]: doDaysName
+    })
+    .then(() => console.log('Save complete'))
+    .catch(error => console.log(`Save faild: ${error}`))
   }
 
   render() {
-    const openDates = this.state.names.reduce((acc, name, i) => ({...acc, [i]:name}))
-
+    // const openDates = this.state.names.reduce((acc, name, i) => ({...acc, [i]:name}))
+    const { openDates } = this.state;
     return (
       <div className="app">
         <header className="header">
           <div className="header__content">        
             <h1>Serviceteam julekalender</h1>
-            <button className="btn">Åpne luke</button>
+            <button onClick={this.openCurrentDate} className="btn">Åpne luke</button>
           </div>
         </header>
         <Canvas />
--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -2,25 +2,15 @@ import React from 'react';
 import Card from './Card';
 class Calendar extends React.Component {
   state = {
-    names: ['boob', 'boo', 'baab'],
     dates: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,24],
     nameDateMap: {}
   };
-
-  componentWillMount() {
-    const { openDates } = this.props;
-    setTimeout(() => {
-      this.setState({
-        nameDateMap: openDates
-      })
-    }, 100)
-  }
 
   render() {
     return (
       <main className="calendar">
         {this.state.dates.map((date, i) => (
-          <Card key={`date-${i}`} number={date} name={this.state.nameDateMap[date]}/>
+          <Card key={`date-${i}`} number={date} name={this.props.openDates[date]}/>
         ))}
       </main>
     );
